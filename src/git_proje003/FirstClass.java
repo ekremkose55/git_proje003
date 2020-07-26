75e8d7c95d0ff74e695ae2287ec78b7a28edd89c
ekremkose55 <67019372+ekremkose55@users.noreply.github.com>
2020-07-25 22:06:54 -0500

    Update FirstClass.java
    Remote masterdan degisiklik

--- src/git_proje003/FirstClass.java
+++ src/git_proje003/FirstClass.java
@@ -5,6 +5,8 @@ public class FirstClass {
 	public static void main(String[] args) {
 		System.out.println("Ilk commit local master branch'den");
 		System.out.println("Ikinci commit local master branch'den");
+		System.out.println("3. commit remote master branch'den");
+		System.out.println("4. commit remote master branch'den");
 
 	}
 

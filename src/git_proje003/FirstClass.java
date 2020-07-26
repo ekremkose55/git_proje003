811bb3cbfdb277397cd7eb9c06d26b7a6f306140
Ekram <ekremkose2001@gmail.com>
2020-07-25 22:29:30 -0500

    harun localden

--- src/git_proje003/FirstClass.java
+++ src/git_proje003/FirstClass.java
@@ -3,11 +3,8 @@ package git_proje003;
 public class FirstClass {
 
 	public static void main(String[] args) {
-		System.out.println("Ilk commit local master branch'den");
-		System.out.println("Ikinci commit local master branch'den");
-
-		System.out.println("Ucuncu commit local master branch'den");
-		System.out.println("Dorduncu commit local master branch'den");
+		System.out.println("Harundan ilk commit local harun branch'den");
+		
 
 		
 	
